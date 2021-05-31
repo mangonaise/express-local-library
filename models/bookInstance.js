366e4ee759757be5d15417aa8897a7af1dcff056
= <liam2robertson@gmail.com>
2021-05-31 20:33:12 +0100

    Add default status to book instance schema

--- models/bookInstance.js
+++ models/bookInstance.js
@@ -6,7 +6,7 @@ const BookInstanceSchema = new Schema(
   {
     book: { type: Schema.Types.ObjectId, ref: 'Book', required: 'true' },
     imprint: { type: String, required: true },
-    status: { type: String, enum: ['available', 'maintenance', 'loaned', 'reserved'], required: true },
+    status: { type: String, enum: ['available', 'maintenance', 'loaned', 'reserved'], default: 'maintenance', required: true },
     due: { type: Date, default: Date.now }
   }
 );
