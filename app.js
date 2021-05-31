5be685b0635aed85942bfe2c9a9424b152d2d4ea
= <liam2robertson@gmail.com>
2021-05-31 13:46:51 +0100

    Set up MongoDB connection

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+const mongoDbUrl = 'mongodb+srv://liam:mongodb@cluster0.z86mb.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
