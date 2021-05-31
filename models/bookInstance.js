1abf5c3b5cbd72b2415a3dd3f9a0270805c01d56
= <liam2robertson@gmail.com>
2021-05-31 14:07:52 +0100

    Create BookInstance model

---
+++ models/bookInstance.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const BookInstanceSchema = new Schema(
+  {
+    book: { type: Schema.Types.ObjectId, ref: 'Book', required: 'true' },
+    imprint: { type: String, required: true },
+    status: { type: String, enum: ['available', 'maintenance', 'loaned', 'reserved'], required: true },
+    due: { type: Date, default: Date.now }
+  }
+);
+
+BookInstanceSchema
+  .virtual('url')
+  .get(() => `/catalog/bookInstance/${this._id}`);
+
+module.exports = mongoose.model('BookInstance', BookInstanceSchema);
